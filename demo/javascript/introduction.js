a4d3f1dd7fbba3c0a6661704e5076bae361cace7
Martin0809 <maning0809@gmail.com>
2014-07-10 09:54:17 +0800

    添加上下翻页

--- demo/javascript/introduction.js
+++ demo/javascript/introduction.js
@@ -1,8 +1,8 @@
 setInterval(function() {
-	var content = document.getElementsByClassName("content")[0];
+	var container = document.getElementsByClassName("container")[0];
 	var boxHeight = document.getElementsByClassName("box")[0].offsetHeight;
-	var contentHeight = content.offsetHeight;
-	content.style.top = (boxHeight-contentHeight)/2 + "px";
+	var containerHeight = container.offsetHeight;
+	container.style.top = (boxHeight-containerHeight)/2 + "px";
 },100);
 
 document.onkeydown = function(e) {
@@ -13,6 +13,11 @@ document.onkeydown = function(e) {
 	var present = document.getElementsByClassName("present")[0];
 	var futures = document.getElementsByClassName("future");
 	var future = futures[0];
+	var tops = document.getElementsByClassName("content-top");
+	var top = tops[tops.length-1];
+	var center = document.getElementsByClassName("content-center")[0];
+	var bottoms = document.getElementsByClassName("content-bottom");
+	var bottom = bottoms[0];
 	if(isie) {
 		key = window.event.keyCode;
 	} else {
@@ -21,6 +26,9 @@ document.onkeydown = function(e) {
 	if(key == "37") {
 		if(pasts.length == 0) {
 			present.style.webkitAnimationName = "noBack";
+			setTimeout(function() {
+				present.style.webkitAnimationName = "none";
+			},1000);
 		} else {
 			present.style.webkitAnimationName = "presentBack";
 			past.style.webkitAnimationName = "pastBack";
@@ -28,14 +36,43 @@ document.onkeydown = function(e) {
 			past.className = "present";
 		}
 	}
+	if(key == "38") {
+		if(tops.length == 0) {
+			center.style.webkitAnimationName = "noUp";
+			setTimeout(function() {
+				center.style.webkitAnimationName = "none";
+			},1000);
+		} else {
+			center.style.webkitAnimationName = "centerBack";
+			top.style.webkitAnimationName = "topBack";
+			center.className = "content-bottom";
+			top.className = "content-center";
+		}
+	}
 	if(key == "39") {
 		if(futures.length == 0) {
 			present.style.webkitAnimationName = "noMove"
+			setTimeout(function() {
+				present.style.webkitAnimationName = "none";
+			},1000);
 		} else {
 			present.style.webkitAnimationName = "presentMove";
 			future.style.webkitAnimationName = "futureMove";
 			present.className = "past";
 			future.className = "present";
+		}
+	}
+	if(key == "40") {
+		if(bottoms.length == 0) {
+			center.style.webkitAnimationName = "noDown";
+			setTimeout(function() {
+				center.style.webkitAnimationName = "none";
+			},1000);
+		} else {
+			center.style.webkitAnimationName = "centerMove";
+			bottom.style.webkitAnimationName = "bottomMove";
+			center.className = "content-top";
+			bottom.className = "content-center";
 		}
 	}
 };
